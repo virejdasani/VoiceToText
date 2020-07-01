4bb6771ee3962cabbc768aaa2c6f360234d92fcd
virejdasani <dasanivirej@gmail.com>
2020-07-01 12:17:54 +0530

    Initial commit

---
+++ app/src/main/java/com/virej/voicetotext/MainActivity.java
@@ -0,0 +1,116 @@
+package com.virej.voicetotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText textViewResult;
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button creatorbutton = (Button) findViewById(R.id.creatorbutton);
+
+        creatorbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent creatorintent = new Intent(MainActivity.this, CreatorActivity.class);
+                startActivity(creatorintent);
+            }
+        });
+
+        Button darkmodebutton = (Button) findViewById(R.id.darkmodebutton);
+        darkmodebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent darkmodeintent = new Intent(MainActivity.this, MainDark.class);
+                startActivity(darkmodeintent);
+            }
+        });
+
+
+        Button copybutton = (Button) findViewById(R.id.copybutton);
+        final ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        //to copy text to clipboard
+        copybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String text = textViewResult.getText().toString();
+            if (!text.equals("")){
+                ClipData clipData = ClipData.newPlainText("text", text);
+                clipboardManager.setPrimaryClip(clipData);
+            }
+
+
+                Toast.makeText(MainActivity.this,"copied",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        textViewResult = (EditText) findViewById(R.id.textViewResult);
+
+
+
+        //admob
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+
+    public void getSpeechInput(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textViewResult.setText(result.get(0));
+                }
+                break;
+        }
+    }
+}
